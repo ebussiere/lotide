7ca9d083b449cf6d16e9a802794411589e692c81
ebussiere <eddy@embtech.ca>
2020-09-16 00:39:41 +0000

    addedEqualsArray

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+let eqArrays = function(arr0,arr1) {
+  assertEqual(arr0.length, arr1.length);
+  if (arr0.length === arr1.length) {
+    for (let i = 0; i < arr0.length; i++) {
+      assertEqual(arr0[i],arr1[i]);
+    }
+  }
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
