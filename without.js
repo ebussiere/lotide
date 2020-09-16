b8c9f40ea8b5228c18cb8729aa884e75c81b9fda
ebussiere <eddy@embtech.ca>
2020-09-16 02:31:33 +0000

    without and assertions

---
+++ without.js
@@ -0,0 +1,29 @@
+const assertArraysEqual = function(actual, expected) {
+  const passMessage = `✅ Assertion Passed: ${actual} === ${expected}`;
+  const failMessage = `🛑 Assertion Failed: ${actual} !== ${expected}`;
+  let message = passMessage;
+  if (actual.length !== expected.length) {
+    message = failMessage;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        message = failMessage;
+      }
+    }
+  }
+  console.log(message);
+};
+
+let without = function(source,itemsToRemove) {
+  for (let i = 0; i < itemsToRemove.length; i++) {
+    for (let j = 0; j < source.length; j++) {
+      if (source[j] === itemsToRemove[i]) {
+        source.splice([j],1);
+      }
+    }
+  }
+  return source;
+};
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
