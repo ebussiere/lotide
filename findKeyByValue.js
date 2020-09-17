85a630a91310ebca8dcf7fc6b5ba45089549e758
ebussiere <eddy@embtech.ca>
2020-09-17 03:35:16 +0000

    findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object,value) {
+  for (let item in object) {
+    if (object[item]) {
+      if (object[item] === value) {
+        return item;
+      }
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sciFi:"The Expanse",
+  comedy:"Brooklyn Nine-Nine",
+  drama:"The Wire",
+  talk:"Ellen",
+  realEntertainment:"Jerry Springer"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Ellen"), "talk");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Jerry Springer"), "realEntertainment");
