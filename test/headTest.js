d659ffbbe2d3bbe82daf8fc18b851f8bc8b2269b
ebussiere <eddy@embtech.ca>
2020-09-21 22:47:09 +0000

    mocha chai added

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,29 @@
-const assertEqual = require('../assertEqual.js');
+//const assertEqual = require('../assertEqual.js');
 const head = require('../head.js');
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([11]), 5);
-assertEqual(head([5]), 5);
-assertEqual(head([]), 5);
+const chai = require('chai');
+let assert = chai.assert;
+
+describe('#head', () => {
+  it('returns 5 for [5, 6, 7]', function() {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it('returns fail for 5 to be head of [11]', () => {
+    assert.notEqual(head([11]), 5);
+  });
+
+  it('returns 5 for [5, 6, 7]', () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it('returns fail for 5 to be head of []', function() {
+    assert.notEqual(head([]), 5);
+  });
+});
+
+
